bd2c143a749a06a0c02fb691da79a277282f5f26
CS-parth <parth.vijy@gmail.com>
2023-09-29 00:53:42 +0530

    Syntax for typeDefs and resolvers their meaning used in GraphQl servers

---
+++ backend-api/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const axios = require('axios');
+const startServer = async ()=>{
+    const port = 8080;
+    const app = express();
+        // Resolvers define how to fetch the types defined in your schema.
+    // This resolver retrieves books from the "books" array above.
+    const resolvers = {
+        Query: {
+            getTodos: async () => {
+                const AxiosURI = 'https://jsonplaceholder.typicode.com/todos';
+                let retData;
+                await axios.get(AxiosURI)
+                .then((res)=>{
+                    retData = res.data;
+                })
+                .catch((err)=>{
+                    console.log("ERROR OCCURED : ", err);
+                })
+                return retData;
+            }
+        },
+    };
+    const typeDefs = `
+        #This is a comment in graphQL
+        type Todo{
+            id : ID!
+            title : String!
+            completed : Boolean!
+        }
+
+        # The "Query" type is special: it lists all of the available queries that
+        # clients can execute, along with the return type for each. In this
+        # case, the "books" query returns an array of zero or more Books (defined above).
+        type Query {
+            getTodos: [Todo]
+        }
+    `;
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    app.use(bodyParser.json());
+    app.use(cors());
+    
+    await server.start();
+    app.use('/graphql', expressMiddleware(server));
+    app.listen(port,()=>{
+        console.log("Server Started at port 8080");
+    })
+}
+
+startServer();
